7ad7cc50a4b01d37da4d120c9d62db544fdc0258
sonu <sonuprajapat2398@gmail.com>
2025-04-03 23:51:30 +0530

    started a new react project mainly focusing on a advance animated UI

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <h1 className="text-5xl text-orange-500 font-bold font-robert-medium">
+      Hello world
+    </h1>
+  )
+}
+
+export default App
