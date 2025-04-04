d1a9685e6a3bdc76d04b4dbc0d7e6e68eab68e7d
sonu <sonuprajapat2398@gmail.com>
2025-04-04 17:22:07 +0530

    Added animation, Created About section , Created Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import About from './components/About'
 import Hero from './components/Hero'
+import NavBar from './components/Navbar'
 
 const App = () => {
   return (
       <main className='relative min-h-screen w-screen overflow-x-hidden'>
-
+          <NavBar />
           <Hero />
+          <About />
     
 
       </main>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,31 +1,99 @@
-import React, { useState, useRef } from 'react'
+import React, { useState, useRef,useEffect } from 'react'
 import Button from './Button';
 import { TiLocationArrow } from 'react-icons/ti';
+import { ScrollTrigger } from "gsap/all";
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+gsap.registerPlugin(ScrollTrigger);
 const Hero = () => {
 
     const [currentIndex,setCurrentIndex] = useState(1)  ;
     const [hasClicked,setHasClicked] = useState(false)  ;
     const [isLoading,setIsLoading] = useState(true)  ;
     const [loadedVideo,setLoadedVideo] = useState(0)  ;
-    const totalVideos = 3  ;
+    const totalVideos = 4  ;
     const nextVideoRef = useRef(null)  ;
     const upcomindVideoIndex = (currentIndex % totalVideos) + 1  ;
 
-    const handleMiniVdClick = () => {
-        setHasClicked(true)  ;
-        setCurrentIndex(upcomindVideoIndex) ;
-    }
 
     const handleVideoLoad = () => {
         setLoadedVideo((prevIndex) => prevIndex + 1)  ;
 
     }
 
+    useEffect(() => {
+        if (loadedVideo === totalVideos - 1) {
+          setIsLoading(false);
+        }
+      }, [loadedVideo]);
+
+    const handleMiniVdClick = () => {
+        setHasClicked(true)  ;
+        setCurrentIndex(upcomindVideoIndex) ;
+    }
+
+   
+
+    useGSAP(
+        () => {
+          if (hasClicked) {
+            gsap.set("#next-video", { visibility: "visible" });
+            gsap.to("#next-video", {
+              transformOrigin: "center center",
+              scale: 1,
+              width: "100%",
+              height: "100%",
+              duration: 1,
+              ease: "power1.inOut",
+              onStart: () => nextVideoRef.current.play(),
+            });
+            gsap.from("#current-video", {
+              transformOrigin: "center center",
+              scale: 0,
+              duration: 1.5,
+              ease: "power1.inOut",
+            });
+          }
+        },
+        {
+          dependencies: [currentIndex],
+          revertOnUpdate: true,
+        }
+      );
+
+      useGSAP(() => {
+        gsap.set("#video-frame", {
+          clipPath: "polygon(14% 0, 72% 0, 88% 90%, 0 95%)",
+          borderRadius: "0% 0% 40% 10%",
+        });
+        gsap.from("#video-frame", {
+          clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+          borderRadius: "0% 0% 0% 0%",
+          ease: "power1.inOut",
+          scrollTrigger: {
+            trigger: "#video-frame",
+            start: "center center",
+            end: "bottom center",
+            scrub: true,
+          },
+        });
+      });
+
     const getVideoSrc = (index) => `videos/hero-${index}.mp4`  ;
 
 
   return (
     <div className="relative h-dvh overflow-x-hidden">
+         {isLoading && (
+        <div className="flex-center absolute z-[100] h-dvh w-screen overflow-hidden bg-violet-50">
+          {/* https://uiverse.io/G4b413l/tidy-walrus-92 */}
+          <div className="three-body">
+            <div className="three-body__dot"></div>
+            <div className="three-body__dot"></div>
+            <div className="three-body__dot"></div>
+          </div>
+        </div>
+      )}
         <div id="video-frame" className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75">
             <div>
                 <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
@@ -54,7 +122,7 @@ const Hero = () => {
                     <video  src={getVideoSrc(currentIndex === totalVideos - 1 ? 1 : currentIndex)}
                             loop
                             muted
-                            //autoPlay
+                            autoPlay
                             className='absolute left-0 top-0 size-full  object-cover object-center'
                             onLoadedData={handleVideoLoad}
                     />
